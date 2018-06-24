cf0986257b5fcc886e1819b834ce348d3dc6bf38
dolefeast <elrois456yt@gmail.com>
2018-06-24 16:49:49 +0200

    Kind of slow

---
+++ problem14.py
@@ -0,0 +1,13 @@
+def collatz_len(n):
+    cont = 0 
+    while n>1:
+        #print(n)
+        if n&1:
+            n = n + n + n + 1
+        else:
+            n//=2
+        cont+=1
+    return cont+1
+top = 10 ** 6
+result = []
+print(max(range(2, top), key=collatz_len))
