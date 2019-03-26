71626eb74298ab0cad2228c15352c169c522b0bf
freierdvogel <santigu10@hotmail.com>
2019-03-26 16:35:15 +0100

    Brute forced problem21

--- problem21.py
+++ problem21.py
@@ -4,9 +4,7 @@ def factor_sum(n):
     return sum([k for i in range(1, int(sqrt(n))+1) if not n%i for k in (i, n//i)])-n
 def is_amicable(n):
     a = factor_sum(n)
-    if a==n:
-        return False
-    return n==factor_sum(a)
+    return n==factor_sum(a) and a != n
 t0 = time()
 top = 10 ** 4
 print(sum([i for i in range(top) if is_amicable(i)]), time()-t0)
